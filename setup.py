c7b99656ef9849380214a2d4b2ed8091d1f7da95
RekGRpth <rekgrpth@gmail.com>
2021-12-21 09:08:04 +0500

    up

--- setup.py
+++ setup.py
@@ -1,28 +1,16 @@
-from distutils.core import setup, Extension
-from distutils.command.build_py import build_py as build_py_orig
-import sys, os
+from setuptools import setup, Extension
 
-class build_ext_first(build_py_orig):
-    def run(self):
-        self.run_command("build_ext")
-        return build_py_orig.run(self)
-
-if sys.platform.startswith("linux"):
-    module = Extension(
-        "_pyhtmldoc",
-        ["pyhtmldoc.i", "pyhtmldoc.c"],
-        include_dirs=[
-            "/usr/local/include/htmldoc",
-        ],
-        libraries=[
-            "htmldoc",
-        ],
-    )
+module = Extension(
+    "_pyhtmldoc",
+    ["pyhtmldoc.i", "pyhtmldoc.c"],
+#    include_dirs=["/usr/local/include/htmldoc"],
+    libraries=["htmldoc"],
+)
 
 setup(
-    name="pyhtmldoc",
-    url="https://github.com/RekGRpth/pyhtmldoc",
-    cmdclass={"build_py": build_ext_first},
     ext_modules=[module],
+    name="pyhtmldoc",
     py_modules=["pyhtmldoc"],
+    url="https://github.com/RekGRpth/pyhtmldoc",
+    version='1.0.0',
 )
